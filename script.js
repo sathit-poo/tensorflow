9fba5de767a3ff0600ac45c2946e69a87b76d575
sathit-poo <71145134+sathit-poo@users.noreply.github.com>
2025-01-30 14:09:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const video = document.getElementById('video')
 var eyeClosedThreshold = 10
 const blinkCount = 0
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('https://sathit-poo.github.io/tensorflow/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('https://sathit-poo.github.io/tensorflow/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://sathit-poo.github.io/tensorflow/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://sathit-poo.github.io/tensorflow/models')
 ]).then(startVideo)
 
 function startVideo() {
